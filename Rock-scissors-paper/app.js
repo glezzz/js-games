7ea9b00fbe72157985ebb86bf5272cb547dbc135
glezzz <alexglezk@gmail.com>
2020-09-10 13:53:01 +0200

    forgot prelunch commit

--- Rock-scissors-paper/app.js
+++ Rock-scissors-paper/app.js
@@ -1,8 +1,7 @@
-(()=>{
 
-    var userChoice = document.getElementsByTagName("button").value;
+let userChoice = document.getElementById("rock").value
 
-    console.log(userChoice)
+    console.log(userChoice);
     // computer
     let computerChoice = Math.random();
     if (computerChoice < 0.34){
@@ -15,40 +14,32 @@
     }
 
     function compare(userChoice, computerChoice) {
-        if (userChoice === computerChoice){
+        if (userChoice === computerChoice) {
             return "it's a tie";
         }
-        if (userChoice === "rock"){
-            if (computerChoice === "scissors"){
+        if (userChoice === "rock") {
+            if (computerChoice === "scissors") {
                 return ("You win");
-            }else if (computerChoice === "paper"){
+            } else if (computerChoice === "paper") {
                 return ("You lose");
 
             }
         }
-        if (userChoice === "paper"){
-            if (computerChoice === "scissors"){
+        if (userChoice === "paper") {
+            if (computerChoice === "scissors") {
                 return ("You lose");
-            }else if (computerChoice === "rock"){
+            } else if (computerChoice === "rock") {
                 return ("You win");
-                }
+            }
         }
-        if (userChoice === "scissors"){
-            if (computerChoice === "rock"){
+        if (userChoice === "scissors") {
+            if (computerChoice === "rock") {
                 return ("You lose");
-            }else if (computerChoice === "paper"){
+            } else if (computerChoice === "paper") {
                 return ("You win");
             }
 
 
+        }
     }
 
-
-
-
-
-
-
-
-
-})();
