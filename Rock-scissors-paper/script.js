75b3fb0eaf124a133e9222d684e6d068e26640ea
glezzz <alexglezk@gmail.com>
2020-09-11 12:46:48 +0200

    scoreboard works

--- Rock-scissors-paper/script.js
+++ Rock-scissors-paper/script.js
@@ -16,10 +16,13 @@ document.getElementById("scissors").addEventListener("click", function () {
 
 })
 
+let userScore = 1;
+let compScore = 1;
 
 // get computer choice & play game
 function playGame() {
 
+    // get computer choice
     let computerChoice = Math.random();
 
     if (computerChoice < 0.34) {
@@ -35,33 +38,40 @@ function playGame() {
         document.getElementById("computer-choice").innerHTML = "Computer picks scissors";
     }
 
+    //play game
     if (userChoice === computerChoice) {
         document.getElementById("result").innerHTML = "It's a draw";
     }
     if (userChoice === "rock") {
         if (computerChoice === "scissors") {
             document.getElementById("result").innerHTML = "You pick rock, you win";
+            document.getElementById("user-score").innerHTML = userScore++;
 
         } else if (computerChoice === "paper") {
             document.getElementById("result").innerHTML = "You pick rock, you lose";
+            document.getElementById("computer-score").innerHTML = compScore++;
 
         }
     }
     if (userChoice === "paper") {
         if (computerChoice === "scissors") {
             document.getElementById("result").innerHTML = "You pick paper, you lose";
+            document.getElementById("computer-score").innerHTML = compScore++;
 
         } else if (computerChoice === "rock") {
             document.getElementById("result").innerHTML = "You pick paper, you win";
+            document.getElementById("user-score").innerHTML = userScore++;
 
         }
     }
     if (userChoice === "scissors") {
         if (computerChoice === "rock") {
             document.getElementById("result").innerHTML = "You pick scissors, you lose";
+            document.getElementById("computer-score").innerHTML = compScore++;
 
         } else if (computerChoice === "paper") {
             document.getElementById("result").innerHTML = "You pick scissors, you win";
+            document.getElementById("user-score").innerHTML = userScore++;
 
         }
 
